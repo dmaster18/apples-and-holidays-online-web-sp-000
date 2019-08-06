f084764eb12b2eeed40fda4fc6c147250dd71b1b
David Strauch <dmaster18@gmail.com>
2019-08-06 18:48:47 +0000

    Automatically backed up by Learn

--- lib/holiday2.rb
+++ lib/holiday2.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
       Thanksgiving: Turkey 
    Spring: 
       Memorial Day: BBQ" 
-  puts output
+  puts output.chomp
 end
 
 all_supplies_in_holidays(holiday_hash)
