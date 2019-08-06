0b9e1553c440f3b24506141b2898feed1f3158cc
David Strauch <dmaster18@gmail.com>
2019-08-06 18:40:26 +0000

    Automatically backed up by Learn

--- lib/holiday2.rb
+++ lib/holiday2.rb
@@ -52,7 +52,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  if 
+  if holiday_hash 
   output = "Winter:
               Christmas: Lights, Wreath
               New Years: Party Hats
