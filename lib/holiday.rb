9cbbaa78d633dff7062c0d2c2f20983b5ef0edb4
David Strauch <dmaster18@gmail.com>
2019-08-06 18:06:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
     season_details.each do |holiday, supplies|
       if holiday == season_details.values.last
         holiday_proper = holiday.to_s.gsub("_", " ").split(" ")
-        holiday_proper.each do |
+        holiday_proper.each do |word|
         print "  " +  + ": " + "\n"
       else 
         print "  " + holiday.to_s.gsub("_", " ").capitalize + ": "
