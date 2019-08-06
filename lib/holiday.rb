a69b78f5cdef01a9af27ceace4eb82b6d0d47de6
David Strauch <dmaster18@gmail.com>
2019-08-06 18:31:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,8 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 
+all_supplies_in_holidays(holiday_hash)
+
 def all_holidays_with_bbq(holiday_hash)
   holiday_name_array = []
   holiday_hash.each do |season, seasonal_data|
