ab893e1a9bc922c986f21f5d63c8c140eae46079
David Strauch <dmaster18@gmail.com>
2019-08-06 18:47:31 +0000

    Automatically backed up by Learn

--- lib/holiday2.rb
+++ lib/holiday2.rb
@@ -54,7 +54,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   output = 
   "Winter: 
-      Christmas: Lights, Wreath \n
+      Christmas: Lights, Wreath 
       New Years: Party Hats 
    Summer: 
       Fourth Of July: Fireworks, BBQ 
