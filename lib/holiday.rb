ff3ac0e9031136349e6c8384e7914eff7a43b0cd
David Strauch <dmaster18@gmail.com>
2019-08-06 18:30:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, season_details|
-    puts season.each
+    puts season
     season_details.each do |holiday, supplies|
       holiday_proper = holiday.to_s.gsub("_", " ").split(" ")
       holiday_proper_final = []
